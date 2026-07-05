ce0a378a77e7ed40e7e4d30e2d531e1845d3bf4f
Hardik Jain Harsora <71239504+Hardik450@users.noreply.github.com>
2026-07-05 21:06:00 +0530

    Delete day-9/preprocessing.py

--- day-9/preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv("df_Orders.csv")
-df2 = pd.read_csv("df_OrderItems.csv")
-
-# Merge the two DataFrames on the 'OrderID' column
-merged_df = pd.merge(df1, df2, on='order_id')
-merged_df.to_csv("order.csv", index=False)
